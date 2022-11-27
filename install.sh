58a3dbee01a6be1a816bc9a4eaaf80ee8cbdd23c
sb-o <sb-o@users.noreply.github.com>
2022-11-27 20:00:46 +0000

    Testing implementing zsh-syntax-highlighting

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "source ${BASEDIR}/.aliases" >> $HOME/.zshrc 
 echo "source ${BASEDIR}/.aliases" >> $HOME/.bashrc
+
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
+echo "source $HOME/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
