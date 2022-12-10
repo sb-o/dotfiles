5eeb52dcc21f15abd91ea988242ae228b97aa673
sb-o <sb-o@users.noreply.github.com>
2022-12-10 23:49:10 +0000

    .zshcustom  and avoid duplicate entries in config

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "source ${BASEDIR}/.aliases" >> $HOME/.zshrc 
-echo "source ${BASEDIR}/.aliases" >> $HOME/.bashrc
+if ! grep -q "source ${BASEDIR}/.aliases" $HOME/.zshrc; then
+    echo "source ${BASEDIR}/.aliases" >> $HOME/.zshrc
+fi
 
+if ! grep -q "source ${BASEDIR}/.aliases" $HOME/.bashrc; then
+    echo "source ${BASEDIR}/.aliases" >> $HOME/.bashrc
+fi
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh-syntax-highlighting
-echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+if ! grep -q "source ${BASEDIR}/.zshcustom" $HOME/.zshrc; then
+    echo "source ${BASEDIR}/.zshcustom" >> $HOME/.zshrc
+fi
+
+if [ ! -d "$HOME/.zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh-syntax-highlighting
+fi
+
+if ! grep -q "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ${ZDOTDIR:-$HOME}/.zshrc; then
+    echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
+
+if [ ! -d "$HOME/.zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh-autosuggestions
+fi
+
+if ! grep -q "source $HOME/.zsh-autosuggestions/zsh-autosuggestions.zsh" ${ZDOTDIR:-$HOME}/.zshrc; then
+    echo "source $HOME/.zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
+
