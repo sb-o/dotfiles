572c567ff8a49330d2c6bee8c52c9fbf6dd533fa
sb-o <12798735+sb-o@users.noreply.github.com>
2023-09-29 20:38:12 +0100

    Updated prepend_to_path spec to use random tmp dir

--- spec/path_spec.sh
+++ spec/path_spec.sh
@@ -1,19 +1,28 @@
 Describe 'prepend_to_path'
     setup() {
-        if [ ! -d "$HOME/.ges9geu0sgue" ]; then
-            mkdir $HOME/.ges9geu0sgue
+        export RANDOM_DIR=$(echo $RANDOM | md5sum | head -c 20; echo);
+
+        # If it fails to generate random directory, then fall back
+        if [ -z "$RANDOM_DIR" ]; then
+            export RANDOM_DIR=a839sfu983uf8s9u39suf89s3uf98su3f
+            
+        # Or if directory already exists
+        elif [ -d "/tmp/$RANDOM_DIR" ]; then
+            export RANDOM_DIR=a839sfu983uf8s9u39suf89s3uf98su3f   
         fi
+
+        mkdir /tmp/$RANDOM_DIR
     }
     cleanup() {
-        export PATH="${PATH#$HOME/.ges9geu0sgue:}"
-        if [ -d "$HOME/.ges9geu0sgue" ]; then
-            rmdir $HOME/.ges9geu0sgue
+        export PATH="${PATH#/tmp/$RANDOM_DIR:}"
+        if [ -d "/tmp/$RANDOM_DIR" ]; then
+            rmdir /tmp/$RANDOM_DIR
         fi
     }
     BeforeAll 'setup'
     AfterAll 'cleanup'
     It 'successfully prepends to path'
-        When call prepend_to_path $HOME/.ges9geu0sgue && [[ "$PATH" == "$HOME/.ges9geu0sgue"* ]]
+        When call prepend_to_path /tmp/$RANDOM_DIR && [[ "$PATH" == "/tmp/$RANDOM_DIR"* ]]
         The output should eq ""
     End
 End
