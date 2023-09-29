7e73037f774a23e89b54e20f269ba6a78824dd45
sb-o <12798735+sb-o@users.noreply.github.com>
2023-09-29 20:42:53 +0100

    Replace random string generation function

--- spec/path_spec.sh
+++ spec/path_spec.sh
@@ -1,6 +1,6 @@
 Describe 'prepend_to_path'
     setup() {
-        export RANDOM_DIR=$(echo $RANDOM | md5sum | head -c 20; echo);
+        export RANDOM_DIR=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 50 | head -n 1); # https://www.markusdosch.com/2022/05/generating-a-random-string-on-linux-macos/
 
         # If it fails to generate random directory, then fall back
         if [ -z "$RANDOM_DIR" ]; then
