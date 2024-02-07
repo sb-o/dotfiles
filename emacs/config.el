f597ce0190d56f5c6cb00bf402c35a7c4dbd9691
sb-o <12798735+sb-o@users.noreply.github.com>
2024-02-07 22:25:07 +0000

    Further emacs config development

--- emacs/config.el
+++ emacs/config.el
@@ -2,6 +2,15 @@
 (let ((file "~/.emacs.d/local.el"))
   (when (file-exists-p file)
     (load file)))
+(defun check-variables-set (variable-list)
+  "Check if variables in VARIABLE-LIST are set and print a warning if not."
+  (dolist (var variable-list)
+    (unless (boundp var)
+      (message "Warning: Variable `%s' is not set" var))))
+
+(check-variables-set '(sb-org-default-file sb-org-agenda-files sb-org-archive-file sb-org-roam-directory sb-org-roam-rootfile))
+
+(setq initial-buffer-choice sb-org-default-file)
 
 ;; PACKAGE MANAGER
 (require 'package)
@@ -20,6 +29,9 @@
 (require 'bind-key)
 
 (setq custom-file "~/.emacs.d/custom.el")
+(unless (file-exists-p custom-file)
+  (with-temp-buffer
+    (write-file custom-file)))
 (load custom-file)
 
 (setq ring-bell-function 'ignore)
@@ -53,6 +65,12 @@
 
 (global-visual-line-mode t)
 
+(global-unset-key (kbd "C-x f"))
+(global-set-key (kbd "C-x f") 'find-file)
+
+(when window-system
+ (set-frame-size (selected-frame) 100 60))
+
 (use-package evil
   :init
   (setq evil-want-C-i-jump nil)
@@ -74,20 +92,22 @@
   (evil-org-agenda-set-keys)
   )
 
-(defvar sb-bookmarks (make-sparse-keymap) "sb Bookmarks")
-(bind-key "C-c a" sb-bookmarks)
-(bind-key "c" (lambda () (interactive) (find-file "~/.emacs.d/config.org")) sb-bookmarks)
-(bind-key "i" (lambda () (interactive) (find-file "~/sb/inbox.org")) sb-bookmarks)
+(load-theme 'deeper-blue)
 
-(which-key-add-key-based-replacements
-  "C-c a" "sb bookmarks"
-  "C-c a c" "config.org"
-  "C-c a i" "inbox.org")
+; Improve readability
+(set-face-attribute 'default nil :height 150) ; Set font
+(setq-default line-spacing 0.25) ; Set line spacing
 
-(use-package kaolin-themes 
-     :config
-     (load-theme 'kaolin-galaxy t)
- )
+; Fixes
+(set-face-attribute 'default nil :foreground "white") ; lighter text
+(set-face-attribute 'shadow nil :foreground "grey90") ; lighter text
+(set-face-attribute 'org-ellipsis nil :foreground "yellow" :underline nil) ; Don't underline ellipsis
+(set-face-attribute 'mode-line nil :background "blue25" :foreground "white")
+(set-face-attribute 'mode-line-inactive nil :background "gray50")
+(set-face-attribute 'mode-line-buffer-id nil :foreground "SkyBlue1")
+
+; Org outline tweaks
+(set-face-attribute 'outline-1 nil :height 155)
 
 (use-package doom-modeline
   :ensure t
@@ -123,21 +143,92 @@
   (setq org-M-RET-may-split-line nil)
   (setq org-clock-string-limit 50)
   (setq org-agenda-files sb-org-agenda-files)
+  ;(setq org-archive-location sb-org-archive-file)
+  (setq org-log-done 'time)
  )
 
 (font-lock-add-keywords 'org-mode
-			'(("^ *\\([-]\\) "
-			   (0 (prog1 () (compose-region   (match-beginning 1) (match-end 1) "•"))))))
+                        '(("^ *\\([-]\\) "
+                           (0 (prog1 () (compose-region   (match-beginning 1) (match-end 1) "•"))))))
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+(setq org-deadline-warning-days 4)
+;(setq org-agenda-span 7)
+(setq org-agenda-start-on-weekday nil)
+
+; More concise "scheduled" and "deadline" descriptors in org-agenda
+(setq org-agenda-scheduled-leaders '("s: " "s.%dx: "))
+(setq org-agenda-deadline-leaders '("d: " "%3dd: " "%2dd ago: "))
+
+; Add new line between each date line
+(setq org-agenda-format-date
+        (lambda (date)
+          (concat "\n" (org-agenda-format-date-aligned date))))
 
 (use-package org-roam
   :custom
   (org-roam-directory sb-org-roam-directory)
+  (org-roam-capture-templates
+   '(("d" "default" plain "%?"
+    :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n#+bibliography: Roam.bib\n")
+    :unnarrowed t)))
   :bind
   (("C-c n l" . org-roam-buffer-toggle)
    ("C-c n f" . org-roam-node-find)
-   ("C-c n g" . org-roam-graph)
+   ;("C-c n g" . org-roam-graph)
    ("C-c n i" . org-roam-node-insert)
-   ("C-c n c" . org-roam-capture))
+   ("C-c n c" . org-roam-capture)
+   ("C-c n r" . (lambda() (interactive)(find-file sb-org-roam-rootfile))))
   )
   :config
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
+
+(use-package org-tidy
+  :config
+ (add-hook 'org-mode-hook #'org-tidy-mode)
+)
+
+(use-package gptel
+  :config
+  (setq gptel-api-key sb-chatgpt-api-key)
+  )
+
+(defun remove-eol-chars ()
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\r" nil t)
+    (replace-match "")))
+
+(bind-key "C-c z" 'gptel-send)
+
+(defvar sb-llm (make-sparse-keymap) "sb LLM")
+(bind-key "C-c x" sb-llm)
+(bind-key "z" 'gptel-menu sb-llm)
+
+(defvar sb-actions (make-sparse-keymap) "sb Actions")
+(bind-key "C-c c" sb-actions)
+(bind-key "i" 'org-indent-block sb-actions)
+(bind-key "m" 'remove-eol-chars sb-actions)
+
+(which-key-add-key-based-replacements
+  "C-c c" "sb actions"
+  )
+
+(defvar sb-views (make-sparse-keymap) "sb Views")
+(bind-key "C-c v" sb-views)
+(bind-key "t" 'org-tidy-toggle sb-views)
+
+(which-key-add-key-based-replacements
+  "C-c v" "sb views"
+)
+
+(defvar sb-bookmarks (make-sparse-keymap) "sb Bookmarks")
+(bind-key "C-c b" sb-bookmarks)
+(bind-key "c" (lambda () (interactive) (find-file "~/.emacs.d/config.org")) sb-bookmarks)
+(bind-key "h" (lambda () (interactive) (find-file "~/sb/Org/homebox.org")) sb-bookmarks)
+(which-key-add-key-based-replacements
+  "C-c b" "sb bookmarks"
+  "C-c b c" "config.org"
+  "C-c b h" "homebox.org"
+  )
--- emacs/custom.el
+++ emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(orderless marginalia which-key consult vertico doom-modeline deep-blue-theme evil-org evil-collection)))
+   '(gptel org-tidy org-roam-ui orderless marginalia which-key consult vertico doom-modeline deep-blue-theme evil-org evil-collection)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
